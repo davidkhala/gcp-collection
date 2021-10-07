da7c8c61182a9270b8e8b04a3a662ea742f32317
David Liu <david.yx.liu@oracle.com>
2021-10-07 04:32:31 -0400

    Create cli.sh

---
+++ cli.sh
@@ -0,0 +1,18 @@
+## The gcloud tool is a part of the Cloud SDK. You must download and install the Cloud SDK on your system and initialize it before you can use the gcloud tool.
+setup(){
+  sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-sdk]
+name=Google Cloud SDK
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+  sudo dnf install google-cloud-sdk
+
+}
+
+
+$1
