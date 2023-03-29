6da77ae533257828c0fa8dd5977d49c1d00c731a
David Liu <david-khala@hotmail.com>
2023-03-29 09:08:45 +0800

    Update gcloud.sh
    Care for macos

--- cli/gcloud.sh
+++ cli/gcloud.sh
@@ -1,30 +1,16 @@
-install() {
-  sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo <<EOM
-[google-cloud-cli]
-name=Google Cloud CLI
-baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=0
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOM
 
-  sudo dnf install -y google-cloud-cli
-
-}
 setup() {
   gcloud init
 
 }
-grant-sdk() {
+grantSdk() {
   # no projectId if auth standalone, you can use `gcloud init` to add projectId into context
   gcloud auth application-default login
 }
-setup-server() {
+setupServer() {
   gcloud init --no-browser
 }
-setup-service-account() {
+setupServiceAccount() {
   gcloud auth activate-service-account --key-file=$1
 }
 logout() {
