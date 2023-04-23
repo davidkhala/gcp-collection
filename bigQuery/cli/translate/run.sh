074e4a432c322cb9e7bbd89b4a1cc97d0b7b3864
David Liu <david-khala@hotmail.com>
2023-04-24 00:47:33 +0800

    Create run.sh

---
+++ bigQuery/cli/translate/run.sh
@@ -0,0 +1,6 @@
+export BQMS_VERBOSE="False"
+export BQMS_MULTITHREADED="True"
+export BQMS_PROJECT="gcp-data-davidkhala"
+export BQMS_GCS_BUCKET="bq-migrate"
+
+./client/examples/teradata/sql/run.sh
