ee8851c63c0994f311c317200335ce92765d9400
David Liu <david.yx.liu@oracle.com>
2023-06-06 17:24:36 +0800

    port back backup-vm

--- compute/cli/gcloud.sh
+++ compute/cli/gcloud.sh
@@ -7,6 +7,10 @@ wait-until-ssh() {
     local instance_name=$1
     curl -sSL https://raw.githubusercontent.com/davidkhala/linux-utils/main/wait-until.sh | bash -s gcloud compute ssh ${instance_name} --zone=${zone} --command=true --quiet
 }
+backup-vm() {
+    local instance_name=$1
+    gcloud compute machine-images create $2 --source-instance=${instance_name}
+}
 delete-vm() {
     local instance_name=$1
     gcloud compute instances delete ${instance_name} --zone=${zone} --quiet
