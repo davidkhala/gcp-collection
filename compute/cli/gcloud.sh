a4b693ccad51ae5f1f63b4e0302ebf6571c5b924
David Liu <david-khala@hotmail.com>
2023-06-06 03:16:25 +0000

    add zone env

--- compute/cli/gcloud.sh
+++ compute/cli/gcloud.sh
@@ -1,5 +1,5 @@
 get-ip() {
     local instance_name=$1
-    gcloud compute instances describe ${instance_name} --format='get(networkInterfaces[0].accessConfigs[0].natIP)'
+    gcloud compute instances describe ${instance_name} --zone=${zone} --format='get(networkInterfaces[0].accessConfigs[0].natIP)'
 }
 $@
