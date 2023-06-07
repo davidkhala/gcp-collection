908b32fdb31b0149938a021484f37be902898205
David Liu <david.yx.liu@oracle.com>
2023-06-07 09:21:40 +0800

    fix

--- compute/cli/vpc.sh
+++ compute/cli/vpc.sh
@@ -3,6 +3,6 @@ create-firewall() {
     local name=$1
     local allowList=${2:-tcp:80,icmp,tcp:22}
     local direction=${3:-INGRESS} # INGRESS | EGRESS
-    gcloud compute firewall-rules create ${name} --allow=tcp:8080 --description="Allow ${direction} traffic on ${allowList}" --direction=${direction}
+    gcloud compute firewall-rules create ${name} --allow=${allowList} --description="Allow ${direction} traffic on ${allowList}" --direction=${direction}
 }
 $@
