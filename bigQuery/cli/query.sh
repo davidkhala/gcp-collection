6fcc4264b5d694bbb5e283b9a99b3214aaee8144
David Liu <david-khala@hotmail.com>
2023-06-28 09:46:56 +0800

    Update query.sh

--- bigQuery/cli/query.sh
+++ bigQuery/cli/query.sh
@@ -3,5 +3,8 @@ translate4CLI() {
     local result=$(cat $1 | sed "s/'/\"/g" | sed '/^[[:blank:]]*--/d;s/--.*//')
     echo "'${result}'"
 }
+schema-of(){
+    bq show --format=prettyjson $1 | jq '.schema.fields'
+}
 
 $@
