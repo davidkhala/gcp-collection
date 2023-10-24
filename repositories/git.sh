2b2f3d3a37700ff5a46691a5576b794077038c02
David Liu <david-khala@hotmail.com>
2023-10-24 17:54:00 +0800

    Update git.sh

--- repositories/git.sh
+++ repositories/git.sh
@@ -1,12 +1,12 @@
 set-git-cookie(){
   local email=$1
-  local password=$2
+  local token=$2
   eval 'set +o history' 2>/dev/null || setopt HIST_IGNORE_SPACE 2>/dev/null
   touch ~/.gitcookies
   chmod 0600 ~/.gitcookies
   git config --global http.cookiefile ~/.gitcookies
   tr , \\t <<\__END__ >>~/.gitcookies
-source.developers.google.com,FALSE,/,TRUE,2147483647,o,git-${email}=${password}
+source.developers.google.com,FALSE,/,TRUE,2147483647,o,git-${email}=1//0${token}
 __END__
   eval 'set -o history' 2>/dev/null || unsetopt HIST_IGNORE_SPACE 2>/dev/null
 
