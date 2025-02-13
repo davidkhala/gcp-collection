6fbecf54205068254c90cd2f42a4c70b70f6a058
David Liu <david-khala@hotmail.com>
2025-02-13 14:32:59 +0800

    davidkhala.gcp.pubsub 0.0.5 fix func signature

--- pubsub/py/tests/test_.py
+++ pubsub/py/tests/test_.py
@@ -1,5 +1,6 @@
 import datetime
 import unittest
+import uuid
 
 from davidkhala.gcp.auth.ci import credential
 from google.cloud.pubsub_v1.futures import Future
@@ -11,16 +12,16 @@ from davidkhala.gcp.pubsub.sub import Sub
 topic_id = 'databricks'
 subscription_id = 'spark'
 auth = credential()
+pub = Pub(topic_id, auth)
 
 
-class TestCase(unittest.TestCase):
-    pub = Pub(topic_id, auth)
+class MessageTestCase(unittest.TestCase):
+
     sub = Sub(subscription_id, topic_id, auth)
 
     def test_pubsub(self):
         print(self.pub.get())
 
-        sub = Sub(subscription_id, topic_id, auth)
         message = "hello world"
 
         def callback(_msg: Message, _future: Future):
@@ -28,7 +29,7 @@ class TestCase(unittest.TestCase):
             _msg.ack()
             _future.cancel()
 
-        future = sub.listen_async(callback)
+        future = self.sub.listen_async(callback)
         self.pub.publish_async(message)
         future.result()
 
@@ -40,6 +41,15 @@ class TestCase(unittest.TestCase):
         self.assertEqual(0, len(self.sub.messages))
         self.sub.purge()
 
-
+class AdminTestCase(unittest.TestCase):
+    def test_sub_lifecycle(self):
+        subscription_id = f"sub{uuid.uuid4().hex}"
+        print(subscription_id)
+        sub = Sub(subscription_id, topic_id, auth)
+        print('start create', datetime.datetime.now())
+        sub.create()
+        print('start delete', datetime.datetime.now())
+        sub.delete()
+        print('end delete', datetime.datetime.now())
 if __name__ == '__main__':
     unittest.main()
