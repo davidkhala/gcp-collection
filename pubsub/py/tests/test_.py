306b6d803bcb6186fc4794013143bd41f07b7e80
David Liu <david-khala@hotmail.com>
2025-02-14 06:57:10 +0800

    naming convention - Cannot use spark app name as sub id

--- pubsub/py/tests/test_.py
+++ pubsub/py/tests/test_.py
@@ -3,6 +3,7 @@ import unittest
 import uuid
 
 from davidkhala.gcp.auth.ci import credential
+from google.api_core.exceptions import InvalidArgument
 from google.cloud.pubsub_v1.futures import Future
 from google.cloud.pubsub_v1.subscriber.message import Message
 
@@ -10,17 +11,16 @@ from davidkhala.gcp.pubsub.pub import Pub
 from davidkhala.gcp.pubsub.sub import Sub
 
 topic_id = 'databricks'
-subscription_id = 'spark'
+id = 'spark'
 auth = credential()
 pub = Pub(topic_id, auth)
 
 
 class MessageTestCase(unittest.TestCase):
-
-    sub = Sub(subscription_id, topic_id, auth)
+    sub = Sub(id, topic_id, auth)
 
     def test_pubsub(self):
-        print(self.pub.get())
+        print(pub.get())
 
         message = "hello world"
 
@@ -30,26 +30,32 @@ class MessageTestCase(unittest.TestCase):
             _future.cancel()
 
         future = self.sub.listen_async(callback)
-        self.pub.publish_async(message)
+        pub.publish_async(message)
         future.result()
 
     def test_purge(self):
-        self.pub.publish(f"{datetime.datetime.now()}")
+        pub.publish(f"{datetime.datetime.now()}")
 
         print(self.sub.messages)
         self.sub.purge()
         self.assertEqual(0, len(self.sub.messages))
         self.sub.purge()
 
+
 class AdminTestCase(unittest.TestCase):
-    def test_sub_lifecycle(self):
-        subscription_id = f"sub{uuid.uuid4().hex}"
-        print(subscription_id)
-        sub = Sub(subscription_id, topic_id, auth)
+    def sub_lifecycle(self, id):
+        print(id)
+        sub = Sub(id, topic_id, auth)
         print('start create', datetime.datetime.now())
         sub.create()
         print('start delete', datetime.datetime.now())
         sub.delete()
         print('end delete', datetime.datetime.now())
+
+    def test_sub_lifecycle(self):
+        self.sub_lifecycle(f"sub{uuid.uuid4().hex}")
+        self.assertRaises(InvalidArgument, lambda: self.sub_lifecycle("Databricks Shell"))
+
+
 if __name__ == '__main__':
     unittest.main()
